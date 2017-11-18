932201f0ee27d5c9a48b2fb119a8391713af2f4f
Erick Jaccoud <ejaccoud@its.jnj.com>
2017-11-17 22:07:55 -0200

    Switching to FPM server API

---
+++ app/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/local/bin/php-fpm-daemon.sh
+
+/usr/local/bin/apache2-foreground
---
+++ app/php/php-fpm-daemon.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/local/php/sbin/php-fpm --fpm-config /usr/local/php/etc/php-fpm.d/www.conf.default -D
